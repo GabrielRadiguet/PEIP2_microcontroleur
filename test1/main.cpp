634bdbc0b49e8ef501287aafd22c03ecf73cfe3e
Gabriel Radiguet <gabriel.radiguet@universite-paris-saclay.fr>
2026-09-23 11:22:32 +0200

    exo2

--- test1/main.cpp
+++ test1/main.cpp
@@ -11,9 +11,15 @@
 AnalogIn pot1(p19);
 AnalogIn pot2(p20);
 
-LM75BD term(p28, p27);
+LM75B term(p28, p27);
+C12832 LCD(p5, p7, p6, p8, p11);
 
-DigitalOut led(LED1);
+DigitalOut led1(LED1);
+DigitalOut led2(LED2);
+DigitalOut led3(LED3);
+DigitalOut led4(LED4);
+
+DigitalOut leds[4] = {led1, led2, led3, led4};
 
 void ResetLeds(){
     for(int i = 0; i < 4; i++) leds[i] = 0;
@@ -28,17 +34,32 @@ void DisplayIntLeds(int i){
     }
 }
 
+int patern[5] = {0, 1, 3, 7, 15};
+
+int AnalogIn2Index(float v, int n){
+    return (int)(v * n);
+}
+
 int main()
 {
-    // Initialise the digital pin LED1 as an output
-    ResetLeds();
+    float temp;
+    float pot1v;
+    float pot2v;
 
-    int i = 0;
-    while (true) {
-        i = ++i % 16;
+    while(true){
+        temp = term.temp();
+        pot1v = pot1.read();
+        pot2v = pot2.read();
 
-        DisplayIntLeds(i);
+        LCD.locate(0, 0);
+        LCD.printf("temp : %f", temp);
 
-        wait_us(500000);
+        LCD.locate(0, LCD.height()/3);
+        LCD.printf("pot1 : %f %d", pot1v, AnalogIn2Index(pot1v, 5));
+
+        LCD.locate(0, 2 * LCD.height()/3);
+        LCD.printf("pot2 : %f", pot2v);
+
+        DisplayIntLeds(patern[AnalogIn2Index(pot1v, 5)]);
     }
 }
